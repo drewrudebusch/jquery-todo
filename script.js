7d613c0d82c1d5aede525bfa32be3abdaa45480f
Drew Rudebusch <drewrudebusch@gmail.com>
2016-03-30 13:04:50 -0700

    Save list items to localStorage, preserve order of items on refresh, persist checkbox for completed items

--- script.js
+++ script.js
@@ -1,5 +1,101 @@
 $(document).ready(function() {
 	var completeTasksHidden = false;
+	var hiddenTasks = [];
+	var visibleTasks = [];
+	var loadSavedTasks = function() {
+	 	if (localStorage.hiddenTasks) {
+		 	var taskList = JSON.parse(localStorage.hiddenTasks);
+			if (taskList) {
+				console.log(JSON.parse(localStorage.hiddenTasks));
+				for(var i = 0; i < taskList.length; i++) {
+					$('#sortable').append('<div class="ui-sortable-handle task-item-container hidden">'
+					    + taskList[i] +
+					  '</div>');
+				};
+			};
+			var taskList = JSON.parse(localStorage.visibleTasks);
+		 	// console.log(taskList)
+			if (taskList) {;
+				for(var i = 0; i < taskList.length; i++) {
+					$('#sortable').append('<div class="ui-sortable-handle task-item-container">'
+					    + taskList[i] +
+					  '</div>');
+				};
+			};
+			$('.task-label').each(function(index, elem){
+				 console.log(elem)
+				 if ($(elem).hasClass('strike')) {
+					$(this).children('.task').prop("checked", true);
+					console.log('success');
+					console.log($(this).children('.task'))
+				};
+			});
+		};
+		if (localStorage.hidden) {
+			completeTasksHidden = JSON.parse(localStorage.hidden);
+		};
+		// clearCompletedTasks();
+	};
+	var createNewTask = function(newTask) {
+		$('#sortable').append(
+			'<div class="ui-sortable-handle task-item-container"><li class="ui-state-default"><div class="checkbox"><label class="task-label"><input type="checkbox" class="task">' + newTask + '</label><div class="delete btn btn-danger"><span class="glyphicon glyphicon-trash"></span></div></div></li></div>');
+	}
+	var clearCompletedTasks = function() {
+		if (completeTasksHidden !== false) {
+			$('.hidden').removeClass('hidden');
+			completeTasksHidden = false;
+		} else {
+			$('.task').each(function(index, elem) {
+				var taskItem = $(this).parents('.task-label')
+				var taskItemContainer = $(this).parents('.task-item-container')
+					if (taskItem.hasClass('strike')) {
+						taskItemContainer.addClass('hidden');
+					};
+				});
+			if($('.hidden').length >= 1) {
+				completeTasksHidden = true;
+			};
+		};
+		pushToLocalStorage();
+	};
+	var sortCompletedToBottom = function() {
+		$('.task').each(function(index, elem) {
+			var taskItem = $(this).parents('.task-label')
+			var taskItemContainer = $(this).parents('.task-item-container')
+				if (taskItem.hasClass('strike')) {
+					taskItemContainer.appendTo('#sortable');
+				};
+			});
+			pushToLocalStorage();
+		};
+	var pushToLocalStorage = function() {
+		$('.task').each(function(index, elem) {
+			var taskItem = $(this).parents('.task-item-container');
+			var taskItemContents = taskItem.html();
+			if(taskItem.hasClass('hidden')) {
+				hiddenTasks.push(taskItemContents);
+			} else {
+				visibleTasks.push(taskItemContents);
+			}
+			
+		});
+		localStorage.setItem('hiddenTasks', JSON.stringify(hiddenTasks));
+		localStorage.setItem('visibleTasks', JSON.stringify(visibleTasks))
+		hiddenTasks = [];
+		visibleTasks = [];
+		storeHiddenTaskStatus();
+	};
+	var storeHiddenTaskStatus = function() {
+		localStorage.setItem('hidden', JSON.stringify(completeTasksHidden));
+	}
+
+	loadSavedTasks();
+	// clearCompletedTasks();
+
+	$('#sortable').on('mouseenter mouseleave', function() {
+		console.log('pushed to localStorage!')
+		pushToLocalStorage();
+	});
 
 	$(function() {
 	    $( "#sortable" ).sortable();
@@ -17,6 +113,10 @@ $(document).ready(function() {
 				if (newTask !== "") {
 					createNewTask(newTask);
 				}
+				pushToLocalStorage();
+				// tasks.push(newTask);
+				// localStorage.setItem('tasks', JSON.stringify(tasks));
+
 				$(elem).val('');
 		});
 	});
@@ -28,50 +128,25 @@ $(document).ready(function() {
 	    } else {
 	    	$(this).parent().removeClass('strike opaque');
 	    }
+	    pushToLocalStorage();
 	});
 
 	$('#sortable').on('click', '.delete', function() {
 		var taskItem = $(this).parents('.task-item-container');
 		taskItem.remove();
+		pushToLocalStorage();
 	});
 
 
 	$('#clear-complete').on('click', function() {
 		clearCompletedTasks();
 		sortCompletedToBottom();
+		pushToLocalStorage();
 	});		
 		
 
-	var clearCompletedTasks = function() {
-		if (completeTasksHidden) {
-			$('.hidden').removeClass('hidden');
-			completeTasksHidden = false;
-		} else {
-			$('.task').each(function(index, elem) {
-				var taskItem = $(this).parents('.task-label')
-				var taskItemContainer = $(this).parents('.task-item-container')
-					if (taskItem.hasClass('strike')) {
-						taskItemContainer.addClass('hidden');
-					};
-				});
-			if($('.hidden').length >= 1) {
-				completeTasksHidden = true;
-			};
-		};
-	};
-
-	var createNewTask = function (newTask) {
-		$('#sortable').append(
-			'<div class="ui-sortable-handle task-item-container"><li class="ui-state-default"><div class="checkbox"><label class="task-label"><input type="checkbox" class="task">' + newTask + '</label><div class="delete btn btn-danger"><span class="glyphicon glyphicon-trash"></span></div></div></li></div>');
-	}
-
-	var sortCompletedToBottom = function () {
-		$('.task').each(function(index, elem) {
-			var taskItem = $(this).parents('.task-label')
-			var taskItemContainer = $(this).parents('.task-item-container')
-				if (taskItem.hasClass('strike')) {
-					taskItemContainer.appendTo('#sortable');
-				};
-			});
-		};
+	
 });
+		// var todoIndex = todos.indexOf($(this).prev().text());
+		// todos.splice(todoIndex,1);
+		// localStorage.setItem('todos', JSON.stringify(todos))
